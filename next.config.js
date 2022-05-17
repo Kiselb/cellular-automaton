bfea171cdef10e731ca962d99a10635845042650
Киселев Михаил В <mvkiselev@group.legion.ru>
2022-05-17 11:59:47 +0300

    Промежуточный commit GitHub Pages settings fo Next.JS 20220517 11:59

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
 const isProd = process.env.NODE_ENV === "production";
 
 module.exports = {
+  basePath: isProd ? "/cellular-automaton" : "",
   eslint: {
     ignoreDuringBuilds: true,
   },
-  pageExtensions: ["page.tsx", "page.ts", "page.jsx", ".page.js"],
+  pageExtensions: ["page.tsx", "page.ts", "page.jsx", "page.js"],
   assetPrefix: isProd ? "/cellular-automaton/" : "",
 };
