6ed837d76b047b28f3be96a91d43abd581440b10
Киселев Михаил В <mvkiselev@group.legion.ru>
2022-05-20 13:49:34 +0300

    Рефакторинг Main.page.tsx 20220520 13:49

--- src/pages/main/Main.page.tsx
+++ src/pages/main/Main.page.tsx
@@ -16,43 +16,62 @@ const Main = ({ mode, probe, onModeChange }: MainProps) => {
     onModeChange(event.currentTarget.value as AppStoreMode);
     !!probe && probe({});
   };
+  if (mode === "Native") {
+    return <MainNative probe={probe} onModeChange={onModeChangeHandler} />;
+  }
+  if (mode === "ReduxThunk") {
+    return (
+      <MainRedux
+        probe={probe}
+        onModeChange={onModeChangeHandler}
+        useSaga={false}
+      />
+    );
+  }
+  if (mode === "ReduxSaga") {
+    <MainRedux probe={probe} onModeChange={onModeChangeHandler} useSaga />;
+  }
+  if (mode === "ReduxEffects") {
+    return <MainEffects probe={probe} onModeChange={onModeChangeHandler} />;
+  }
+  return <MainNative probe={probe} onModeChange={onModeChangeHandler} />;
 
-  return (
-    <>
-      {(() => {
-        switch (mode) {
-          case "Native":
-            return (
-              <MainNative probe={probe} onModeChange={onModeChangeHandler} />
-            );
-          case "ReduxThunk":
-            return (
-              <MainRedux
-                probe={probe}
-                onModeChange={onModeChangeHandler}
-                useSaga={false}
-              />
-            );
-          case "ReduxSaga":
-            return (
-              <MainRedux
-                probe={probe}
-                onModeChange={onModeChangeHandler}
-                useSaga
-              />
-            );
-          case "ReduxEffects":
-            return (
-              <MainEffects probe={probe} onModeChange={onModeChangeHandler} />
-            );
-          default:
-            return (
-              <MainNative probe={probe} onModeChange={onModeChangeHandler} />
-            );
-        }
-      })()}
-    </>
-  );
+  // return (
+  //   <>
+  //     {(() => {
+  //       switch (mode) {
+  //         case "Native":
+  //           return (
+  //             <MainNative probe={probe} onModeChange={onModeChangeHandler} />
+  //           );
+  //         case "ReduxThunk":
+  //           return (
+  //             <MainRedux
+  //               probe={probe}
+  //               onModeChange={onModeChangeHandler}
+  //               useSaga={false}
+  //             />
+  //           );
+  //         case "ReduxSaga":
+  //           return (
+  //             <MainRedux
+  //               probe={probe}
+  //               onModeChange={onModeChangeHandler}
+  //               useSaga
+  //             />
+  //           );
+  //         case "ReduxEffects":
+  //           return (
+  //             <MainEffects probe={probe} onModeChange={onModeChangeHandler} />
+  //           );
+  //         default:
+  //           return (
+  //             <MainNative probe={probe} onModeChange={onModeChangeHandler} />
+  //           );
+  //       }
+  //     })()}
+  //   </>
+  // );
 };
 
 export default Main;
